c0d1358f363ac3a61ed7af1461eabc48f54838af
richardpark-msft <richardpark-msft>
2025-09-05 16:26:23 -0700

    Adding in a few ideas for functions that we could use to generate Go compatible package names, which take into account the Service namespace..
    Some things that could use some thought:
    - Do we like passing "arguments" to my lambda using some space separated stuff in the input string? I'm not a Mustache person, so maybe there's some mustachey way to do this in a more elegant way
    - How primitive should primitives be? `rejoin` is basically `replace` if you specify the entire range '0,'. It can be equivalent of 'split', so that's probably evidence it's doing too much.

--- packages/compiler/src/init/file-templating.ts
+++ packages/compiler/src/init/file-templating.ts
@@ -24,6 +24,49 @@ export type FileTemplatingContext = Omit<InitTemplate, "libraries"> &
      * Normalize package name for languages other than C#. It replaces `.` with `-` and toLowerCase
      */
     normalizePackageName: () => (text: string, render: any) => string;
+
+    // more lower level primitives, need to nest a few calls to get what we need.
+    // Example, using a combo of slice and replace (and the existing toLowerCase)
+    /*
+      parameters.ServiceNamespace == "Azure.Messaging.EventGrid.SystemEvents"
+
+      package-dir: "{{#toLowerCase}}az{{#slice}}-1, . {{parameters.ServiceNamespace}}{{/slice}}{{/toLowerCase}}"
+      service-dir: "{{#toLowerCase}}{{#replace}}. / sdk/{{#slice}}1,-1 . {{parameters.ServiceNamespace}}{{/slice}}{{/replace}}{{/toLowerCase}}"
+
+      becomes
+
+      service-dir: "sdk/messaging/eventgrid"
+      package-dir: "azsystemevents"
+    */
+
+    /**
+     * Splits the text apart, based on delimiter, and then returns the segments (rejoined)
+     * based on their slice indices.
+     */
+    slice: () => (text: string, render: any) => string;
+
+    /**
+     * Replaces the first instance of a substring with another string.
+     */
+    replace: () => (text: string, render: any) => string;
+
+    // slightly higher level one that slices and dices. Saves us on some nested calls.    
+    /*
+      parameters.ServiceNamespace == "Azure.Messaging.EventGrid.SystemEvents"
+
+      service-dir: "sdk/{{#toLowerCase}}{{#rejoin}}. / 1,-1 {{parameters.ServiceNamespace}}{{/rejoin}}{{/toLowerCase}}"
+      package-dir: "az{{#toLowerCase}}{{#rejoin}}. / -1 {{parameters.ServiceNamespace}}{{/rejoin}}{{/toLowerCase}}"
+
+      becomes
+
+      service-dir: "sdk/messaging/eventgrid"
+      package-dir: "azsystemevents"
+    */
+
+    /**
+     * Splits text apart and rejoins specified segments using a new delimiter.
+     */
+    rejoin: () =>  (text: string, render: any) => string;
   };
 
 export interface CasingUtils {
@@ -41,6 +84,9 @@ export function createFileTemplatingContext(config: ScaffoldingConfig): FileTemp
     normalizeVersion,
     toLowerCase,
     normalizePackageName,
+    slice,
+    replace,
+    rejoin,
     casing,
   };
 }
@@ -76,3 +122,86 @@ const normalizePackageName = function () {
     return render(text).replaceAll(".", "-").toLowerCase();
   };
 };
+
+const slice = function () {
+  return function (text: string, render: any): string {
+    // we're assuming that the args are space separated and that the behavior here is
+    // the same as the JavaScript slice function.
+    //
+    // 1st arg is the start-index,end-index
+    // 2nd arg is the delimiter
+    // 3rd arg is the string to split
+    text = render(text);
+
+    const args = text.split(" ", 3);
+
+    if (args.length !== 3) {
+      throw new Error(`split requires 3 arguments, got ${args.length} in "${text}"`);
+    }
+
+    const [start, end] = args[0].split(',', 2);
+
+    const delimiter = args[1];
+    const actualText = args[2];
+
+    const endIdx: number | undefined = end !== "" ? parseInt(end, 10) : undefined;
+
+    return actualText.split(delimiter)
+      .slice(
+        parseInt(start, 10), 
+        endIdx)
+      .join(delimiter);
+  };
+};
+
+const replace = function () {
+  return function (text: string, render: any): string {
+    // we're assuming that the args are space separated and that the behavior here is
+    // the same as the JavaScript slice function.
+    //
+    // 1st arg is the text to search for
+    // 2nd arg is the text to replace it with
+    // 3rd arg is the string to search and replace on.
+    const args = render(text).split(" ", 3);
+
+    if (args.length !== 3) {
+      throw new Error(`replace requires 3 arguments, got ${args.length} in "${text}"`);
+    }
+
+    const [search, replace, actualText] = args;
+    return actualText.replace(search, replace);
+  };
+};
+
+const rejoin = function () {
+  return function (text: string, render: any): string {
+    // we're assuming that the args are space separated and that the behavior, for indices, is
+    // the same as the JavaScript slice function. 
+    //
+    // NOTE: since we use space as the delimiter for the 'args', we can't accept 'space' as 
+    // a before/after delimiter. I believe this is going to be a rare case.
+    //
+    // 1st arg is the original delimiter
+    // 2nd arg is the new delimiter, after rejoining
+    // 3rd arg is the slice() compatible args for which segments to keep
+    // 4th arg is the string to search and replace on.
+    text = render(text);
+
+    const args = text.split(" ", 4);
+
+    if (args.length !== 4) {
+      throw new Error(`split requires 4 arguments, got ${args.length} in "${text}"`);
+    }
+
+    const oldDelimiter = args[0];
+    const newDelimiter = args[1];
+    const [start, end] = args[2].split(',', 2);
+    const actualText = args[3];
+
+    const endIdx: number | undefined = end !== "" && end != null ? parseInt(end, 10) : undefined;
+
+    return actualText.split(oldDelimiter)
+      .slice(parseInt(start, 10), endIdx)
+      .join(newDelimiter);
+  };
+}
